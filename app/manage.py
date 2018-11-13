a7db3f60ea8a80274d52dd0273615e6dcda89af2
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-13 19:48:30 +0300

    [Delivers  #161926021] add get a specific parcel api endpoint

--- app/manage.py
+++ app/manage.py
@@ -9,26 +9,6 @@ class Parcel:
     def __init__(self):
         self.parcels = parcels
 
-    def get_all_parcels(self):
-        """get all parcels."""
-        return self.parcels
-        
-
-    def add_parcel(self, sender_id, location, name, phone, country, destination, weight, price):
-        """add new parcel."""
-        parcel = {
-            "id": len(self.parcels)+1,
-            "sender_id": sender_id,
-            "pickup_location": location,
-            "recepient_name": name, 
-            "recepient_phone": phone, 
-            "country":country, 
-            "destination": destination,
-            "parcel_weight": weight,
-            "parcel_price": price,
-            "status": "Pending",
-            "created": str(datetime.now())
-        }
-        self.parcels.append(parcel)
-        return parcel
-
+    def search_parcel(self, id):
+        parcel = [parcel for parcel in self.parcels if parcel['id'] == int(id)]
+        return parcel 
