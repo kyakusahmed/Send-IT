86ed8af07b42e05c105d25434b1033a6b353a69f
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-13 22:33:00 +0300

    update files

--- app/manage.py
+++ app/manage.py
@@ -9,15 +9,45 @@ class Parcel:
     def __init__(self):
         self.parcels = parcels
 
+    def get_all_parcels(self):
+        """get list of all parcels."""
+        return self.parcels
+
+    def add_parcel(self, sender_id, location, name, phone, country, destination, weight, price):
+        """add new parcel."""
+        parcel = {
+            "id": len(self.parcels)+1,
+            "sender_id": sender_id,
+            "pickup_location": location,
+            "recepient_name": name, 
+            "recepient_phone": phone, 
+            "country":country, 
+            "destination": destination,
+            "parcel_weight": weight,
+            "parcel_price": price,
+            "status": "Pending",
+            "created": str(datetime.now())
+        }
+        self.parcels.append(parcel)
+        return parcel
+
+    def search_parcel(self, id):
+        parcel = [parcel for parcel in self.parcels if parcel['id'] == int(id)]
+        return parcel 
+
     def search_sender_parcels(self, sender_id):
         parcels = [parcel for parcel in self.parcels if parcel['sender_id'] == int(sender_id)]
         return parcels
 
+    def get_parcel(self, id):
+        return self.search_parcel(id)
+
     def update_parcel_status(self, id, status):
-            parcel = self.search_parcel(id)
+        parcel = self.search_parcel(id)
         if parcel:
             parcel[0].update({"status": status})
             return parcel
         return None  
+
 
 
