720b0ae21b7eb4b7695c4ea61b82dfe1f2270e88
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-20 16:22:39 +0300

    add tests files 162029679

---
+++ tests/base_test.py
@@ -0,0 +1,35 @@
+import unittest
+import json
+from app.views.user import app2
+from app.views.admin import app2
+from app.models.migrations import Migration
+from .import user_register, admin_register, admin_login, user_login
+
+
+class BaseTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.migration = Migration()
+        self.app1 = app2.test_client()
+        
+    
+    # def create_tables():
+    #     self.migration.create_tables([users, parcels])
+
+    def return_admin_token(self):
+        """admin token."""
+        self.app1.post('/api/v1/admins/register', json=admin_register)
+        response = self.app1.post('/api/v1/admins/login', json=admin_login)
+        return json.loads(response.data)['access_token']
+
+    def return_user_token(self):
+        """user token."""
+        self.app1.post('/api/v1/users/register', json=user_register)
+        response = self.app1.post('/api/v1/users/login', json=user_login)
+        data = json.loads(response.data)
+        return json.loads(response.data)['access_token']
+
+    # def tearDown(self):
+    #     self.migration.drop_tables([users, parcels])
+
+    
---
+++ tests/test_admin.py
@@ -0,0 +1,128 @@
+import json
+from .base_test import BaseTest
+
+class AdminTest(BaseTest):
+       
+    
+    def test_update_status(self):
+        token = self.return_user_token()
+        admin_token = self.return_admin_token()
+        parcel= {
+
+                "user_id": 1,
+                "sender_name" : "ahmad kyakulumbye",
+                "sender_phone" : "256706196611",
+                "pickup_location" : "busabala road-zone 1",
+                "recepient_name" : "muwonge badru",
+                "recepient_phone":"254704196613",
+                "recepient_country":"kenya",
+                "destination":"nairobi-main street-plot 20",
+                "weight": "50kg",
+                "price":"500shs",
+                "status":"pending"
+                }
+        self.app1.post('/api/v1/parcels', headers={"Authorization": "Bearer " + token}, json=parcel)
+        response = self.app1.put('/api/v1/parcels/1/status',headers={"Authorization": "Bearer " + admin_token},
+            json={"status": "delivered"})
+        assert response.status_code == 200
+        assert json.loads(response.data)['parcel'] == "status updated"    
+
+    def test_update_current_location(self):
+        data = {
+            "user_id": 1,
+            "sender_name" : "ahmad kyakulumbye",
+            "sender_phone" : "256706196611",
+            "pickup_location" : "busabala road-zone 1",
+            "recepient_name" : "muwonge badru",
+            "recepient_phone":"254704196613",
+            "recepient_country":"kenya",
+            "destination":"nairobi-main street-plot 20",
+            "weight": "50kg",
+            "price":"500shs",
+            "status":"pending"
+        }
+        self.app1.post('/api/v1/parcels',content_type="application/json", data=json.dumps(data))
+        data = {"current_location":"eldoret"}
+        response = self.app1.put('/api/v1/parcels/1',content_type="application/json", data=json.dumps(data))
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['msg'], "Missing Authorization Header")
+        assert response.status_code == 401  
+
+    def test_get_all_parcels(self):
+        response = self.app1.get('/api/v1/parcels/admin')
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['msg'], "Missing Authorization Header")
+        assert response.status_code == 401 
+
+    def test_update_status_without_token(self):  
+        data = {
+            "user_id": 1,
+            "sender_name" : "ahmad kyakulumbye",
+            "sender_phone" : "256706196611",
+            "pickup_location" : "busabala road-zone 1",
+            "recepient_name" : "muwonge badru",
+            "recepient_phone":"254704196613",
+            "recepient_country":"kenya",
+            "destination":"nairobi-main street-plot 20",
+            "weight": "50kg",
+            "price":"500shs",
+            "status":"pending"
+        }
+        self.app1.post('/api/v1/parcels',content_type="application/json", data=json.dumps(data))
+        data = {"status":"cancelled"}
+        response = self.app1.put('/api/v1/parcels/1',content_type="application/json", data=json.dumps(data))
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['msg'], "Missing Authorization Header")
+        assert response.status_code == 401    
+
+    def test_update_user_to_admin_without_token(self):
+        data = {
+            "user_id": 1,
+            "sender_name" : "ahmad kyakulumbye",
+            "sender_phone" : "256706196611",
+            "pickup_location" : "busabala road-zone 1",
+            "recepient_name" : "muwonge badru",
+            "recepient_phone":"254704196613",
+            "recepient_country":"kenya",
+            "destination":"nairobi-main street-plot 20",
+            "weight": "50kg",
+            "price":"500shs",
+            "status":"pending"
+        }
+        self.app1.post('/api/v1/parcels',content_type="application/json", data=json.dumps(data))
+        data = {"roles":"admin"}
+        response = self.app1.put('/api/v1/parcels/1',content_type="application/json", data=json.dumps(data))
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['msg'], "Missing Authorization Header")
+        assert response.status_code == 401
+
+    def test_get_user_not_found(self):
+        response = self.app1.post('/api/v1/users/roles/1234')
+        data = json.loads(response.get_data(as_text=True)) 
+        self.assertEqual(data['msg'], "user not found")
+
+    def test_role_is_required(self):
+        data = {
+            "user_id": 1,
+            "sender_name" : "ahmad kyakulumbye",
+            "sender_phone" : "256706196611",
+            "pickup_location" : "busabala road-zone 1",
+            "recepient_name" : "muwonge badru",
+            "recepient_phone":"254704196613",
+            "recepient_country":"kenya",
+            "destination":"nairobi-main street-plot 20",
+            "weight": "50kg",
+            "price":"500shs",
+            "status":"pending"
+        }
+        self.app1.post('/api/v1/parcels',content_type="application/json", data=json.dumps(data))
+        data = {"roles":""}
+        response = self.app1.put('/api/v1/users/roles/1',content_type="application/json", data=json.dumps(data))
+        data = json.loads(response.get_data(as_text=True)) 
+        self.assertEqual(data['msg'], "Role is required"), 400
+
+
+
+
+
+
